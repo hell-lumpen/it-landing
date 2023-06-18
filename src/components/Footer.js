d17454fd98b5419df47660a851b97d8acfbd35e7
Grubenko Maxim <mdgrubenko@maieducation>
2023-06-18 19:09:43 +0300

    add Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,13 +8,13 @@ function Footer() {
       <footer>
         <p>© {new Date().getFullYear()} IT-центр МАИ. Все права защищены.</p>
         <div className="social-media-icons" style={socialMediaIconsStyles}>
-          <a href="https://vk.com/itmai" style={iconLinkStyles}>
+          <a href="https://vk.com/itmai" style={iconLinkStyles} target="_blank" rel="noopener noreferrer">
             <img src={vk} alt="VK" height={30} width={30} style={iconStyles} />
           </a>
-          <a href="https://t.me/itcmai" style={iconLinkStyles}>
+          <a href="https://t.me/itcmai" style={iconLinkStyles} target="_blank" rel="noopener noreferrer">
             <img src={telegram} alt="Telegram" height={30} width={30} style={iconStyles} />
           </a>
-          <a href="mailto:itcentrmai@gmail.com" style={iconLinkStyles}>
+          <a href="mailto:itcentrmai@gmail.com" style={iconLinkStyles} target="_blank" rel="noopener noreferrer">
             <img src={mail} alt="Email" height={30} width={30} style={iconStyles} />
           </a>
         </div>
