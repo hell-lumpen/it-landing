33a401ce30756d37b1f3395bb31a1a485740d34c
Grubenko Maxim <mdgrubenko@maieducation>
2023-06-21 18:47:17 +0300

    add slider styles

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,30 +1,56 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
+import '../styles/ImageSlider.css'
+
 
 const ImageSlider = ({ items, sliderOptions }) => {
+  const [slidesToShow, setSlidesToShow] = useState(1);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setSlidesToShow(3);
+      } else {
+        setSlidesToShow(1);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // вызываем функцию для установки изначального значения
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   const settings = {
     dots: sliderOptions.dots !== undefined ? sliderOptions.dots : true,
     infinite: sliderOptions.infinite !== undefined ? sliderOptions.infinite : true,
     speed: sliderOptions.speed || 500,
-    slidesToShow: sliderOptions.slidesToShow || 1,
     slidesToScroll: sliderOptions.slidesToScroll || 1,
     autoplay: sliderOptions.autoplay !== undefined ? sliderOptions.autoplay : true,
     autoplaySpeed: sliderOptions.autoplaySpeed || 3000,
+    slidesToShow: slidesToShow,
   };
 
+
   return (
-      <Slider {...settings}>
-        {items.map((item, index) => (
-            <div key={index}>
-              <img src={item.image}  alt={item.image} width={310} height={'auto'}/>
-              {item.title && <h3>{item.title}</h3>}
-              {item.text && <p>{item.text}</p>}
-            </div>
-        ))}
-      </Slider>
+      <div className="slider-container">
+        <div className="slider-wrapper">
+          <Slider {...settings}>
+            {items.map((item, index) => (
+                <div key={index} className="slider-item">
+                  <img src={item.image} alt={`Image ${index + 1}`} />
+                  {item.title && <h3>{item.title}</h3>}
+                  {item.text && <p>{item.text}</p>}
+                </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
   );
 };
 
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -128,7 +128,21 @@ function MainPage() {
     dots: false,
     autoplay: true,
     autoplaySpeed: 2000,
-    slidesToShow: 3
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
 
   return (
